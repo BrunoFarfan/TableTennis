487a9d0fcde8dc0cf6af4ef7277977abf08621af
Santiago Larrain <santiago.larrain@uc.cl>
2023-09-14 10:42:57 -0300

    Código funcional para modelar lanzamiento

--- Code/sistema_dinamico.py
+++ Code/sistema_dinamico.py
@@ -3,39 +3,32 @@ import numpy as np
 
 class PingPong():
 
-    def __init__(self, x0, w0):
+    def __init__(self, x0= np.array([0,0,0]), Rw= np.array([0,0,0])):
+        # Ball constants
         self.m     = 0.0027    # Masa de pelota en kg
         self.r     = 0.0200    # Radio de pelota en m
-        self.R     = 0.0400    # Radio de rodillos en m
-        self.phi   = 180       # Rotación de sistema en 360°
-        self.ytrig = np.array([np.cos(np.deg2rad(self.phi)), -np.sin(np.deg2rad(self.phi + 30)), -np.cos(np.deg2rad(self.phi + 60))])
-        self.ztrig = np.array([np.sin(np.deg2rad(self.phi)), np.cos(np.deg2rad(self.phi + 30)), -np.sin(np.deg2rad(self.phi + 60))])
-        
-        self.Cr0 = 0.47
-        self.Cr  = 0.47   # Coeficiente de roce de aire
-        self.CM0 = 0.1
-        self.CM  = 0.1    # Constante de Magnus (0.1-0.5)
-        
-        self.rho = 1.2928   # Densidad del aire en kg/m3
-        self.g   = 9.87     # Constante de gravedad
-        
-        # Velocidad angular de la pelota
-        self.W = np.array([0,0,0])
-        self.A = np.array([
-            [-self.Cr, -self.KM()*self.W[2], self.KM()*self.W[1]],
-            [self.KM()*self.W[2], -self.Cr, -self.KM()*self.W[0]],
-            [-self.KM()*self.W[1], self.KM()*self.W[0], -self.Cr]
-        ])
-        self.B = np.array([0,0,-self.m*self.g])    # Fuerzas conservativas
+        self.R     = 0.0650    # Radio de rodillos en m
 
+        # Coefficients        
+        self.CD  = 0.45        # Air friction coefficient
+        self.rho = 1.2928      # Aire density in kg/m3
+        self.g   = 9.87        # Earth's gravity
 
-        # Variables de estado
-        self.x0     = x0                 # Starting point
-        self.w0     = w0                 # Roller's speed
-        self.x      = np.array([0,0,0])  #  x,  y,  z
-        self.x_dot  = np.array([0,0,0])  # vx, vy, vz
-        self.x_ddot = np.array([0,0,0])  # ax, ay, az
+        self.pitch = np.deg2rad(29)
+        self.yaw = np.deg2rad(0)
 
+        # State variables
+        self.x0     = x0                 # Start position of ball      [m]
+        self.Rw     = Rw*2*np.pi/60      # Roller's speed              [Rad/s]
+        self.w      = np.array([0,0,0])  # Angular velocity of ball    [Rad/s]
+        self.x      = np.array([0,0,0])  # Current position of ball    [m]
+        self.x_dot  = np.array([0,0,0])  # Linear velocity of ball     [m/s]
+        self.x_ddot = np.array([0,0,0])  # Linear acceleration of ball [m/s2]
+
+        # Force enablers
+        self.gravity_en = True
+        self.drag_en    = True
+        self.magnus_en  = True
         self.deltaT = 0.001
 
         self.reset()
@@ -43,27 +36,45 @@ class PingPong():
     def reset(self):
         # Back to origin
         self.x = self.x0
+        # starting linear speed [m/s]
+        self.x_dot = self.rotate(np.array([self.R/3 * (self.Rw[0] + self.Rw[1] + self.Rw[2]),0,0]))
+        # starting angular speed [Rad/s]
+        self.w = self.rotate(np.array([0, np.dot(self.Rw, np.array([-1,0.5,0.5])), np.dot(self.Rw, np.array([0,-np.sqrt(3)/2,np.sqrt(3)/2]))]))
 
-        # linear speed [m/s]
-        self.x_dot = np.array([self.r * np.mean(self.w0),0,0])
-        # angular speed [Rad/s]
-        self.W = np.array([0, np.dot(self.w0, self.ytrig), np.dot(self.w0, self.ztrig)])
-        # Reset A
-        self.update_A()
+    def rotate(self, x):
+        R = np.array([[ np.cos(self.pitch)*np.cos(self.yaw), np.cos(self.pitch)*np.sin(self.yaw), np.sin(self.pitch)],
+                      [                   -np.sin(self.yaw),                     np.cos(self.yaw),                   0],
+                      [-np.sin(self.pitch)*np.cos(self.yaw), -np.sin(self.pitch)*np.sin(self.yaw), np.cos(self.pitch)]])
+        Rinv = np.linalg.inv(R)
+        return np.dot(Rinv, x)
+
+    def Fgravity(self):
+        if self.gravity_en:
+            return np.array([0,0,-self.m*self.g])
+        else:
+            return np.array([0,0,0])
     
-    def KM(self):
-        return 1/2 * self.CM * self.rho * (4 * np.pi * self.r**2) * self.r 
+    def Fdrag(self):
+        lin_vel = np.linalg.norm(self.x_dot)
+        A = np.pi * self.r**2
+        if self.drag_en:
+            return - 1/2 * self.rho * A * self.CD * lin_vel * self.x_dot
+        else:
+            return np.array([0,0,0])
 
-    def update_A(self):
-        self.A = np.array([
-            [-self.Cr, -self.KM()*self.W[2], self.KM()*self.W[1]],
-            [self.KM()*self.W[2], -self.Cr, -self.KM()*self.W[0]],
-            [-self.KM()*self.W[1], self.KM()*self.W[0], -self.Cr]
-        ])
+    def Fmagnus(self):
+        lin_vel = np.linalg.norm(self.x_dot)
+        ang_vel = np.linalg.norm(self.w)
+        CL = 0.3187*(1 - np.exp(-0.002483 * ang_vel))
+        A = np.pi * self.r**2
+        if self.magnus_en:
+            return 1/2 * CL * self.rho * A * lin_vel * (np.cross(self.w, self.x_dot) / ang_vel)
+        else:
+            return np.array([0,0,0])
 
     def step(self, t):
         # Actualizar variables de estado
-        self.x_ddot = np.dot(self.A, self.x_dot) + np.transpose(self.B)
+        self.x_ddot = 1/self.m * (self.Fgravity() + self.Fdrag() + self.Fmagnus())
         self.x_dot = self.x_dot + self.x_ddot*t
         self.x = self.x + self.x_dot*t
     
@@ -71,9 +82,9 @@ class PingPong():
         xaxis = []
         yaxis = []
         zaxis = []
-        xaxis.append(x0[0])
-        yaxis.append(x0[1])
-        zaxis.append(x0[2])
+        xaxis.append(self.x[0])
+        yaxis.append(self.x[1])
+        zaxis.append(self.x[2])
         hit = False
         while not hit:
             self.step(self.deltaT)
@@ -85,7 +96,7 @@ class PingPong():
                 # Break when the ball hits the table
                 hit = True
         
-        return xaxis, yaxis, zaxis        
+        return np.round(xaxis, 10), np.round(yaxis, 10), np.round(zaxis, 10)        
 
     def std_shots(self):
         # Initiate plot
@@ -93,33 +104,28 @@ class PingPong():
         ax.grid()
 
         # Ideal
-        self.Cr = 0
-        self.CM = 0
+        self.drag_en   = False
+        self.magnus_en = False
         self.reset()
         xaxis, yaxis, zaxis = self.simulate()
         ax.plot3D(xaxis, yaxis, zaxis)
+        print(xaxis[-1])
         
         # Friction
-        self.Cr = self.Cr0
+        self.drag_en = True
         self.reset()
         xaxis, yaxis, zaxis = self.simulate()
         ax.plot3D(xaxis, yaxis, zaxis)
+        print(xaxis[-1])
 
-        # Top-Spin
-        self.CM = self.CM0
-        self.w0 = np.array([30,60,60])
+        # Spin
+        self.magnus_en = True
         self.reset()
-        print(self.W)
         xaxis, yaxis, zaxis = self.simulate()
-        ax.plot3D(xaxis, yaxis, zaxis)        
+        ax.plot3D(xaxis, yaxis, zaxis)
+        print(xaxis[-1])
         
-        # # Back-Spin
-        # self.w0 = np.array([120,15,15])
-        # self.reset()
-        # xaxis, yaxis, zaxis = self.simulate()
-        # ax.plot3D(xaxis, yaxis, zaxis)
-        
-        ax.legend(["Ideal", "Friction", "Top-Spin", "Back-spin"]) #, "Left-Spin", "Right-spin"])
+        ax.legend(["Ideal", "Friction", "Spin"])#, "Reverse-spin"]) #, "Left-Spin", "Right-spin"])
         ax.set_title("Standard throws")
         ax.set_xlabel('x')
         ax.set_ylabel('y')
@@ -128,12 +134,22 @@ class PingPong():
 
 if __name__ == '__main__':
     
-    x0 = np.array([0,0,0.3])
-    w0 = np.array([50,50,50])
-    ball = PingPong(x0, w0)
+    start_pos     = np.array([0,0,0.47])     # [m]
+    rollers_speed = np.array([330,330,330])    # [RPM]
+    ball = PingPong(x0= start_pos, Rw= rollers_speed)
 
     ball.std_shots()
-    
+    # max_distance = (0,0)
+    # for b in range(-15,16):
+    #     ball.yaw = np.deg2rad(b)
+    #     for a in range(90):
+    #         ball.pitch = np.deg2rad(a)
+    #         ball.reset()
+    #         x, y, z = ball.simulate()
+    #         output = (np.linalg.norm(np.array([x[-1], y[-1], z[-1]])) , a)
+    #         max_distance = max(output, max_distance, key= lambda x : x[0])
+    # print(max_distance)
+
 
 
 
