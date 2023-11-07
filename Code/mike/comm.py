39dfa5736fb1c96920dc74d28492d9ed181438d0
Bruno Farfán <bruno.farfan.miquel@uc.cl>
2023-11-06 23:48:34 -0300

    funciona enviar angulo y codigo base para los faulhabers escrito (probablemente este malo)

--- Code/mike/comm.py
+++ Code/mike/comm.py
@@ -3,17 +3,48 @@ import serial
 
 
 class Comunicador:
-    def __init__(self, puerto="COM7", baudrate=115200, timeout=3):
-        self.comunicador = serial.Serial(port=puerto, baudrate=baudrate, timeout=timeout)
+    def __init__(self, puerto_arduino="COM7", puerto_faulhaber0="COM3",
+                 puerto_faulhaber1="COM4", puerto_faulhaber2="COM5", baudrate=115200, timeout=3):
+        
+        self.arduino = serial.Serial(port=puerto_arduino, baudrate=baudrate, timeout=timeout)
+        self.faulhabers = [
+            serial.Serial(port=puerto_faulhaber0, baudrate=115200, timeout=timeout),
+            serial.Serial(port=puerto_faulhaber1, baudrate=115200, timeout=timeout),
+            serial.Serial(port=puerto_faulhaber2, baudrate=115200, timeout=timeout)
+        ]
+
+        self.start_faulhabers()
+
         self.angulo_anterior = 0
     
 
-    def enviar_angulo(self, angulo, espera=0.8):
+    def enviar_angulo(self, angulo, espera=0.3):
         if angulo != None:
             time.sleep(espera)
-            msgOn = f"{round(angulo)}"
+            msgOn = f"{round(angulo)}\n"
             if abs(angulo - self.angulo_anterior) > 1:
                 msgEncode = str.encode(msgOn)
-                self.comunicador.write(msgEncode)
+                self.arduino.write(msgEncode)
 
                 self.angulo_anterior = angulo
+
+
+    def start_faulhabers(self):
+        msgOn = str.encode("en\n")
+        for faulhaber in self.faulhabers:
+            msgEncode = str.encode(msgOn)
+            faulhaber.write(msgEncode)
+
+    
+    def stop_faulhabers(self):
+        msgVel = str.encode("v0\n")
+        msgOff = str.encode("di\n")
+        for faulhaber in self.faulhabers:
+            msgEncode = str.encode(msgVel)
+            faulhaber.write(msgEncode)
+            msgEncode = str.encode(msgOff)
+            faulhaber.write(msgEncode)
+
+    
+    def enviar_velocidad(self, velocidades, espera=0.3):
+        pass
--- Code/mike/main.py
+++ Code/mike/main.py
@@ -13,7 +13,7 @@ def enviar_angulo():
 
 video = Camara(numero_camara=1, rango=np.array([5, 50, 50]))
 
-comunicador = Comunicador()
+comunicador = Comunicador(puerto="COM5")
 
 comm_handler = th.Thread(target=enviar_angulo, daemon=True)
 comm_handler.start()
